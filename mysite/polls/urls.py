e33dc0b69882130754fcc0e4d35012963af13d8a
umhaneul <asd7g123@naver.com>
2020-05-06 09:47:23 +0900

    polls/urls app_name

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
-from . import views
+from . import  views
+
+app_name = 'polls'
 
 urlpatterns = [
-    # 127.0.0.1.8000/polls
+    #127.0.0.1:8000/polls/
     path('', views.index, name='index'),
-    # 127.0.0.1.8000/polls/5/
+    #127.0.0.1:8000/polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
-    # 127.0.0.1.8000/polls/5/results/
+    #127.0.0.1:8000/polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
-    # 127.0.0.1.8000/polls/5/vote/
+    #127.0.0.1:8000/polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
